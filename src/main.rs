34974a53075adb289f2452075c08a23783b2dd04
cblairy <corentinblairy@outlook.com>
2026-09-26 13:18:37 +0200

    feat(UI): :sparkles: Add desktop UI for detected Dofus windows

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 // main.rs
 mod autoswitch;
 mod capture;
+mod gui;
 mod shortcut_biding;
 pub mod window;
 
-use anyhow::Result;
-use std::thread;
-
-fn main() -> Result<()> {
-    let (conn, _) = x11rb::connect(None)?;
-
-    thread::spawn(move || {
-        autoswitch::auto_switch(&conn);
-    })
-    .join()
-    .map_err(|_| anyhow::anyhow!("Autoswitch thread Panic"))?;
-
-    Ok(())
+fn main() -> eframe::Result<()> {
+    gui::run()
 }
